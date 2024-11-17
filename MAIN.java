cb66874773ca60b38c2ccc7e75bbfe6a96eeb777
Martinho777 <martinguille17@gmail.com>
2024-11-17 17:49:31 -0600

    Create MAIN.java

---
+++ MAIN.java
@@ -0,0 +1,14 @@
+from tkinter import*
+from tkinter import messagebox
+from db_connection import get_connection
+from usuario import Usuario
+from mesa import Mesa
+from tkinter import Canvas
+from PIL import Image, ImageTk  
+from random import randint as ran
+from QR import*
+from tkinter import filedialog
+from menu import Menu
+from cuenta import Cuenta
+import multiprocessing
+from promocion import *
